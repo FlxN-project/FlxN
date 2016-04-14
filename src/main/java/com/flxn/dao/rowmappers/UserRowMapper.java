c8e5a2e48887c126a3df4bed582082f444d822c9
VoronovAA <woronmey@gmail.com>
2016-04-14 15:53:15 +0300

    usermapper

---
+++ src/main/java/com/flxn/dao/rowmappers/UserRowMapper.java
@@ -0,0 +1,19 @@
+package com.flxn.dao.rowmappers;
+
+import com.flxn.dao.model.User;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * Created by X8 on 14.04.2016.
+ */
+public class UserRowMapper implements RowMapper<User> {
+
+    @Override
+    public User mapRow(ResultSet resultSet, int i) throws SQLException {
+        User user=new User(resultSet.getInt("USER_ID"),resultSet.getString("EMAIL"),resultSet.getString("PASSWORD"));
+        return user;
+    }
+}
