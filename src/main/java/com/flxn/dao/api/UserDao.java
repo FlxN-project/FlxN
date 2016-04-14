f6ddc14723a28813d33dfb3b517cc4b9e9a728d6
VoronovAA <woronmey@gmail.com>
2016-04-14 15:52:18 +0300

    user impl

--- src/main/java/com/flxn/dao/api/UserDao.java
+++ src/main/java/com/flxn/dao/api/UserDao.java
@@ -6,6 +6,7 @@ import com.flxn.dao.model.User;
  * Created by X8 on 29.03.2016.
  */
 public interface UserDao extends GenericDao<User> {
+ boolean exist (int id);
  boolean exist(String email);
  String INSERT_USER="INSERT INTO \"USERS\" VALUES(DEFAULT,?,?)";
  String UPATE_USER_BY_ID="UPDATE \"USERS\" SET  \"EMAIL\"=?, \"PASSWORD\"=? WHERE \"USER_ID\"=?  ";
---
+++ src/main/java/com/flxn/dao/impl/UserDaoImpl.java
@@ -0,0 +1,65 @@
+package com.flxn.dao.impl;
+
+import com.flxn.dao.api.UserDao;
+import com.flxn.dao.model.User;
+import com.flxn.dao.rowmappers.UserRowMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.sql.SQLException;
+
+
+/**
+ * Created by X8 on 13.04.2016.
+ */
+public class UserDaoImpl implements UserDao {
+    @Autowired
+    @Qualifier("jdbcTemplate")
+    private JdbcTemplate jdbcTemplate;
+
+    @Override
+    public boolean exist(int id) {
+        boolean result=jdbcTemplate.queryForObject(SELECT_BY_EMAIL,new Object[]{id},Integer.class)!=0;
+        return result;
+    }
+
+    @Override
+    public boolean exist(String email) {
+            boolean result=jdbcTemplate.queryForObject(SELECT_BY_EMAIL,new Object[]{email},Integer.class)!=0;
+            return result;
+    }
+    @Override
+    public User create(User object) {
+        if(!exist(object.getEmail()))
+        {
+            jdbcTemplate.queryForObject(INSERT_USER,new Object[]{object.getEmail(),object.getPassword()},new UserRowMapper());
+            return object;
+         }
+        return null;
+    }
+
+    @Override
+    public void delete(User object) {
+    }
+
+    @Override
+    public void update(User object) {
+        if(exist(object.getId())){
+            jdbcTemplate.update(UPATE_USER_BY_ID,new Object[]{object.getEmail(),object.getPassword()});
+        }
+        else
+            throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public User getById(int id) {
+        if (exist(id)){
+            User user=jdbcTemplate.queryForObject(SELECT_BY_ID,new Object[]{id},new UserRowMapper());
+            return user;
+        }
+        else {
+            return null;
+        }
+    }
+}
